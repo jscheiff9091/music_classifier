20ffb4d7ab0712a5e7993933e38466c445fc0373
Luke <lukebarbier12@gmail.com>
2019-11-07 07:16:01 -0700

    experimenting with clipping in dtft

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from __future__ import division
 from scipy.io import wavfile
 from mfcc import *
 from os import listdir
+import numpy as np
+import matplotlib.pyplot as plt
 
 def problem1():
     print("Problem 1")
     last_time = 24
+    window_size = 2048
+    fft_size = 1000 #?
 
     audio_signals = {}
 
     # Load wavs
     for wav in listdir("wavs"):
-        if "wav" in wav: # make sure it's a wav file
+        if wav == "chroma.wav": # ignore chroma
+            pass
+        elif "wav" in wav: # make sure it's a wav file
             fs, data = wavfile.read("wavs/" + wav)
             audio_signals[wav] = [fs, data]
 
@@ -19,9 +25,55 @@ def problem1():
     for wav in audio_signals:
         last_index = get_index_of_time(audio_signals[wav][0], last_time)
         audio_signals[wav][1] = audio_signals[wav][1][:last_index]
+
+    # Calculate mfcc's
+    for wav in audio_signals:
+        print("MFCC's : " + wav)
+        fs, data = audio_signals[wav]
+        compute_mfccs(data, fs, fft_size, window_size)
     
 def get_index_of_time(fs, time):
     return int( fs * time )
+
+def test_fft():
+    t = np.linspace(0, 0.5, 500)
+    s = np.sin(40 * 2 * np.pi * t)
+    # plt.ylabel("Amp")
+    # plt.xlabel("Time [s")
+    # plt.plot(t,s)
+    # plt.show()
+    fft = np.fft.fft(s)
+    T = t[1] - t[0] # sample period should be 0.1
+    print(T)
+    N = s.size
+
+    f = np.linspace(0, 1/T, N)
+
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frequency [Hz]")
+    plt.bar(f[:N // 2], np.abs(fft)[:N // 2] * 1/N, width=1.5)  # 1 / N is a normalization factor
+    plt.show()
+
+    # I should do the dtft and then put in time to see if I hear clipping
+
+def test_clipping():
+    print("Test clipping")
+    last_time = 24
+    window_size = 2048
+    fft_size = 1000 #?
+
+    audio_signals = {}
+
+    # Load wavs
+    fs, data = wavfile.read("wavs/chroma.wav")
+    compute_mfccs(data, fs, fft_size, window_size, tapered=False)
+
+    # load one of the songs, perform fft in time, perform ifft and play back -> clipping?
+    # then test if we apply the hamming window
+
+
     
 if __name__ == "__main__":
-    problem1()
+    # test_fft()
+    test_clipping()
+    # problem1()
--- mfcc.py
+++ mfcc.py
@@ -1,60 +1,84 @@
+from __future__ import division
 import numpy as np
 
-def compute_mfccs(audio_sig, fs, fft_size, window_size=2048):
-    # dtft_windows = fft_window(audio_sig, fs, fft_size, window_size)
+MIN_FREQ = 20
+# MAX_FREQ depends on each signal's fs = {fs/2}
 
-    # ... fft_window()
-    # loop
-    # ... mfcc()
-    # append to our matrix
+def compute_mfccs(audio_sig, fs, fft_size, window_size=2048, tapered=True):
+    # dtft_windows = fft_window(audio_sig, fs, fft_size, window_size)
+    dtft_windows = np.array(())
+    for i in range( int(len(audio_sig) / window_size) - 1):
+        start_index = i*window_size
+        end_index = (i+1)*window_size
+        if tapered:
+            pass
+        else:
+            signal_window = get_windowed_audio_no_taper(audio_sig, start_index, end_index)
+        dtft = fft_window(signal_window).reshape((window_size,1))
+        print(dtft.shape)
+        break # TODO append to an array and play back # TODO call mfccs()
+        if not dtft_windows:
+            dtft_windows = dtft
+        else:
+            np.append(dtft_windows, dtft)
+        
+
+def get_windowed_audio_no_taper(audio_sig, start_index, end_index):
+    return audio_sig[start_index:end_index]
+
+def get_audio_window_tapered_hamming(audio_sig, start_index, end_index):
+    # I think extract the window then do point wise multiplication with the hamming window
     pass
 
-def mfcc(fft_coefs, Nb=40):
+def mfcc(fft_coefs, fft_size, Nb=40):
     """
-    Computes the mfcc coefficients for a 24s track snipit
+    Computes the mfccs for a 24s track snipit
 
     params
     ------
     fft_coefs : matrix {}
-        fourier transform coefficeints of the track at T=24s
+        fourier transform coefficeints of the track up to T=24s
+    fft_size : int
+        Spacing in between linear frequencies from 0 to fs/2
     Nb : int
         number of filters
 
     returns
     -------
-    mfcc coefs : matrix { 40 x 258 }
+    mfccs : matrix { 40 x 258 }
         Mel Frequency Cepstral Coefficients
     """
+    hop_size = fft_size / 2
     f_max = fs/2
-    mel_min = np.log10(1 + 20/700)
+    mel_min = np.log10(1 + MIN_FREQ/700)
     mel_max = np.log10(1 + f_max/700)
     mel_range = (mel_max - mel_min) / Nb
 
 
-def fft_window(audio_sig, fs, fft_size, window_size=2048):
+def fft_window(audio_sig):
     """
-    Computes the fourier transform of the signal in each window
+    Computes the fourier transform of the signal
 
     https://docs.scipy.org/doc/numpy/reference/routines.fft.html
 
     params
     ------
-    audio_sig : 1D - vector
+    audio_sig : numpy array 1D
         audio signal
-    fs : int
-        sampling frequency
-    fft_size : int
-        Size of our fft?
-    window_size : int
-        Number of samples in each window
-    num_frames : int
-        Number of overlapping frames in the signal
-        Denoted Nf in the document
     
     returns
     -------
-    dtft_windows : matrix { }
-    """
-    
+    dtft : array {1 x 2048}
+        Symetric array around N/2
+        let N = len(audio_sig)
+        Indices: value
+        0: 0 freq
+        1: sample freq / N
+        2: 2*sample_freq / N
+        N/2: (repeat) 0 freq copy
+        N/2+1: (repeat) sample freq / N
+        ...
 
-    # do something with numpy.fft( ... ) NICE it defaults to one-sided
+        to just access one half do dtft[:N//2] (assuming you have N, the signal window size)
+    """
+    return np.fft.fft(audio_sig)
