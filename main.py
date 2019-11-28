29bb2fb18de63055df2f64c6016e4ee64a4a1465
Luke Barbier <lukebarbier12@gmail.com>
2019-11-27 17:24:12 -0700

    problem2 init

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def problem1():
     print("Problem 1")
     last_time = 24
     window_size = 2048
-    fft_size = 1000 #? Not sure
 
     audio_dict = {}
 
@@ -62,7 +61,6 @@ def problem1():
         # im = ax.matshow(C, cmap=cm.gray_r, norm=LogNorm(vmin=0.01, vmax=1))
         plt.gca().invert_yaxis()
         plt.show()
-
     
 def get_index_of_time(fs, time):
     return int( fs * time )
@@ -174,10 +172,69 @@ def test_no_clipping(): # TODO move these to another source file
 
 def test_create_filter():
     create_filter_bank(22050, 40)
+
+def problem2():
+    print("Problem 2")
+    last_time = 24
+    window_size = 2048
+
+    audio_dict = {}
+
+    # Load wavs
+    # for wav in listdir("wavs"):
+    #     if wav == "chroma.wav": # ignore chroma
+    #         pass
+    #     elif "wav" in wav: # make sure it's a wav file
+    #         fs, data = wavfile.read("wavs/" + wav)
+    #         audio_dict[wav] = [fs*2, data] # wavfile.read returns fs/2 (for some reason...)
+
+    # Start out with just chroma
+    fs, data = wavfile.read("wavs/chroma.wav")
+    audio_dict[wav] = [fs*2, data] # wavfile.read returns fs/2 (for some reason...)
+
+    # Trim signals to 24s
+    for wav in audio_dict:
+        last_index = get_index_of_time(audio_dict[wav][0], last_time)
+        audio_dict[wav][1] = audio_dict[wav][1][:last_index]
+
+    fs = 22050
+    # Calculate mfcc's
+    for wav in audio_dict:
+        print("Getting pitch for: " + wav)
+        _fs, data = audio_dict[wav]
+        mfccs = compute_mfccs(filter_bank, data, window_size)
+        # print(mfccs.shape)
+        # print(mfccs)
+        ymin = np.min(mfccs)
+        ymax = np.max(mfccs)
+        # print(ymin)
+        # print(ymax)
+        # return
+        # f = plt.figure()
+        fig, ax = plt.subplots(1,1)
+
+        num_windows = mfccs.shape[1]
+
+        img = ax.imshow(mfccs, norm=LogNorm(vmin=1.0, vmax=1e9))
+        ax.set_xticks([0, int(0.25 * num_windows), int(0.5*num_windows), int(0.75*num_windows), num_windows])
+        x_label_list = [0, int(0.25*last_time), int(0.5*last_time), int(0.75*last_time), last_time]
+
+        ax.set_xticklabels(x_label_list)
+        # plt.imshow(mfccs)
+        
+        plt.xlabel("time (seconds)")
+        plt.ylabel("filter #")
+        title = wav[:-4] + " MFCC's"
+        plt.title(title)
+        # fig.colorbar(img, orientation="horizontal")
+        # im = ax.matshow(C, cmap=cm.gray_r, norm=LogNorm(vmin=0.01, vmax=1))
+        plt.gca().invert_yaxis()
+        plt.show()
     
 if __name__ == "__main__":
     # test_fft()
     # test_clipping()
     # test_no_clipping()
-    problem1()
+    # problem1()
     # test_create_filter()
+    problem2()
